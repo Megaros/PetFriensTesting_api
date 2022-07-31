1a7306b8a2e90e17206ee5dbcf8db99badf3514a
Олег К <megaros22@gmail.com>
2022-07-31 13:19:50 +0300

    Добавлено 2 позитивных теста на наличие животных в том числе своих при условии валидной авторизации.

--- api.py
+++ api.py
@@ -12,6 +12,7 @@ class PetFrands:
             'password': password
         }
         res = requests.get(self.base_url + 'api/key', headers=headers)
+        print(res)
         status = res.status_code
         result = ''
         try:
@@ -21,9 +22,23 @@ class PetFrands:
         return status, result
 
     def get_list_of_pets(self, auth_key, filter):
-        headers = {'auth_key': auth_key}
+        """Метод делает GET запрос к API сервера и возвращает статус и результат в формате JSON
+        cо списком питомцев"""
+        headers = {'auth_key': auth_key['key']}
         filter = {'filter': filter}
         res = requests.get(self.base_url+'api/pets', headers=headers, params=filter)
+        status = res.status_code
+        try:
+            result = res.json()
+        except:
+            result = res.text
+        return status, result
+    def get_list_of_pets_my_pets(self, auth_key, filter):
+        """Метод делает GET запрос к API сервера и возвращает статус и результат в формате JSON
+        cо списком моих питомцев"""
+        headers = {'auth_key': auth_key['key']}
+        filter = {'filter': filter}
+        res = requests.get(self.base_url + 'api/pets', headers=headers, params=filter)
         status = res.status_code
         try:
             result = res.json()
--- tests/test_pet_friends.py
+++ tests/test_pet_friends.py
@@ -8,4 +8,15 @@ def test_get_api_key_valid_user(email = valid_email, password = valid_password):
     assert status == 200
     assert 'key' in result
 
-def test_get_list_of_pets()
+def test_get_all_pets_with_valid_key(filter = ''):
+    _, auth_key = pf.get_api_key(valid_email, valid_password)
+    status, result = pf.get_list_of_pets(auth_key, filter)
+    assert status == 200, 'Авторизация не прошла'
+    assert len(result['pets']) > 0, 'Список животных пуст'
+    print('Всего животных', len(result['pets']), 'шт')
+def test_get_my_pets_with_valid_key(filter ='my_pets'):
+    _, auth_key = pf.get_api_key(valid_email, valid_password)
+    status, result = pf.get_list_of_pets(auth_key, filter)
+    assert status == 200, 'Авторизация не прошла'
+    assert len(result['pets']) > 0, 'Список животных пуст'
+    print('Моих животных',len(result['pets']), 'шт')
